c6b2277a389a0fee23a78adfda42c97b41ed7973
Nathan Randall <data-douser@github.com>
2024-03-26 16:14:23 -0600

    Fixes and tests for scan checkpoint feature

--- pkg/scanner/scanner_test.go
+++ pkg/scanner/scanner_test.go
@@ -7,7 +7,6 @@ import (
 
 	git "github.com/go-git/go-git/v5"
 	gitmemory "github.com/go-git/go-git/v5/storage/memory"
-	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/Pii-Hole-Engineering/no-phi-ai/pkg/cfg"
@@ -17,12 +16,14 @@ import (
 )
 
 var (
-	test_empty_repository_map = make(map[string]*ScanRepository)
-	test_context              = context.Background()
-	test_failed_msg           = "failed test : %s"
-	test_log_level            = "trace"
-	test_work_dir             = "/tmp/no-phi-ai/test/pkg/scanner"
-	test_valid_config_func    = func() *cfg.Config {
+	test_context           = context.Background()
+	test_failed_msg        = "failed test : %s"
+	test_log_level         = "trace"
+	test_repo_name         = "learn"
+	test_repo_org          = "BlackIQ"
+	test_repo_url          = "git@github.com:" + test_repo_org + "/" + test_repo_name + ".git"
+	test_work_dir          = "/tmp/no-phi-ai/test/pkg/scanner"
+	test_valid_config_func = func() *cfg.Config {
 		c := cfg.NewDefaultConfig()
 		c.App.Log.Level = test_log_level
 		c.AzureAI.AuthKey = "test-auth-key"
@@ -41,6 +42,7 @@ var (
 // TestNewScanner unit test function tests the NewScanner() function.
 func TestNewScanner(t *testing.T) {
 	t.Parallel()
+
 	tests := []struct {
 		config_func  func() *cfg.GitConfig
 		ctx          context.Context
@@ -95,11 +97,21 @@ func TestNewScanner(t *testing.T) {
 			if test.err_expected {
 				assert.Errorf(t, err, test_failed_msg, test.name)
 				assert.Nilf(t, s, test_failed_msg, test.name)
-			} else {
-				assert.NoErrorf(t, err, test_failed_msg, test.name)
-				if assert.NotNilf(t, s, test_failed_msg, test.name) {
-					assert.NotEqualf(t, "", s.ID, test_failed_msg, test.name)
-				}
+				return
+			}
+			assert.NoErrorf(t, err, test_failed_msg, test.name)
+			if !assert.NotNilf(t, s, test_failed_msg, test.name) {
+				assert.FailNowf(t, "failed to create scanner : %s", err.Error())
+			}
+			assert.NotEqualf(t, "", s.ID, test_failed_msg, test.name)
+			if !assert.NotNil(t, s.chan_commits, "Scanner.chan_commits should not be nil") {
+				assert.FailNow(t, "Scanner.chan_commits should not be nil")
+			}
+			if !assert.NotNil(t, s.chan_errors, "Scanner.chan_errors should not be nil") {
+				assert.FailNow(t, "Scanner.chan_errors should not be nil")
+			}
+			if !assert.NotNil(t, s.chan_requests, "Scanner.chan_requests should not be nil") {
+				assert.FailNow(t, "Scanner.chan_requests should not be nil")
 			}
 		})
 	}
@@ -176,84 +188,6 @@ func TestScanner_Scan(t *testing.T) {
 			}
 
 			// TODO
-		})
-	}
-}
-
-// TestScanner_addScanRepository tests the addScanRepository method of the Scanner
-// object type.
-func TestScanner_addScanRepository(t *testing.T) {
-	test_expected_map_func_empty := func() map[string]*ScanRepository {
-		return test_empty_repository_map
-	}
-	t.Parallel()
-	tests := []struct {
-		name              string
-		repo              *ScanRepository
-		expected_err      error
-		expected_map_func func() map[string]*ScanRepository
-	}{
-		{
-			name: "TestRepoValid",
-			repo: &ScanRepository{
-				ID:   test_repo_url,
-				Name: test_repo_name,
-				URL:  test_repo_url,
-			},
-			expected_err: nil,
-			expected_map_func: func() map[string]*ScanRepository {
-				out := make(map[string]*ScanRepository)
-				out[test_repo_url] = &ScanRepository{
-					ID:   test_repo_url,
-					Name: test_repo_name,
-					URL:  test_repo_url,
-				}
-				return out
-			},
-		},
-		{
-			name:              "NilRepoError",
-			repo:              nil,
-			expected_err:      errors.Wrap(ErrScannerAddScanRepositoryNil, ErrMsgAddScanRepository),
-			expected_map_func: test_expected_map_func_empty,
-		},
-		{
-			name: "RepoNameError",
-			repo: &ScanRepository{
-				ID:   "",
-				Name: test_repo_name,
-				URL:  test_repo_url,
-			},
-			expected_err:      errors.Wrap(ErrScannerAddScanRepositoryEmptyID, ErrMsgAddScanRepository),
-			expected_map_func: test_expected_map_func_empty,
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			scanner, err := NewScanner(
-				test_context,
-				test_valid_git_config_func(),
-				memory.NewMemoryResultRecordIO(test_context),
-			)
-			if !assert.NoError(t, err) {
-				assert.FailNow(t, "failed to create scanner")
-			}
-
-			err = scanner.addScanRepository(test.repo)
-
-			// validate the expected error
-			if test.expected_err == nil {
-				assert.NoError(t, err)
-				// test the getScanRepository() method
-				get_repo, get_err := scanner.getScanRepository(test.repo.ID)
-				assert.NoError(t, get_err)
-				assert.Equal(t, test.repo, get_repo)
-			} else {
-				assert.Equal(t, test.expected_err.Error(), err.Error())
-			}
-			// validate the expected map of repositories
-			assert.Equal(t, test.expected_map_func(), scanner.scan_repositories)
 		})
 	}
 }
@@ -357,28 +291,11 @@ func TestScanner_processResponse(t *testing.T) {
 	repository, init_err := git.Init(gitmemory.NewStorage(), nil)
 	assert.NoError(t, init_err)
 
-	scan_repo, err := NewScanRepository(NewScanRepositoryInput{
-		ChannelErrors:   make(chan<- error),
-		ChannelRequests: make(chan<- rrr.Request),
-		Config:          test_repo_scan_config,
-		Context:         test_context,
-		Repository:      repository,
-		URL:             test_repo_url,
-	})
+	assert.NotNil(t, repository, "scanner repository pointer should not be nil")
+	s.repository = repository
 	// is_scan_complete must be set in order to ensure that the
 	// processResponse method does not block indefinitely
-	scan_repo.is_scan_complete = true
-
-	if !assert.NoError(t, err) || !assert.NotNil(t, scan_repo) {
-		assert.FailNow(t, "failed to create test repository for Scanner")
-	}
-
-	if add_err := s.addScanRepository(scan_repo); !assert.NoError(t, add_err) {
-		assert.FailNow(t, "failed to add test repository to Scanner")
-	}
-	if !assert.NotEmpty(t, scan_repo.ID) {
-		assert.FailNow(t, "failed to create test repository with non-empty ID")
-	}
+	s.is_scan_complete = true
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
@@ -401,18 +318,13 @@ func TestScanner_processResponse(t *testing.T) {
 			assert.Truef(t, req_key_exists, "failed to find response ID in requests tracker : ID=%s", response.ID)
 			assert.Equal(t, req_key_data.Code, tracker.KeyCodeComplete)
 
-			test_scan_repo, test_scan_repo_err := s.getScanRepository(scan_repo.ID)
-			if !assert.NoError(t, test_scan_repo_err) {
-				assert.FailNow(t, "failed to get test repository from scanner")
-			}
-
-			commit_key_data, commit_key_exists := test_scan_repo.TrackerCommits.Get(response.Commit.ID)
+			commit_key_data, commit_key_exists := s.TrackerCommits.Get(response.Commit.ID)
 			assert.Truef(t, commit_key_exists, "failed to find commit ID in commits tracker : ID=%s", response.Commit.ID)
 			assert.Contains(t, commit_key_data.Children, response.Object.ID)
 			assert.Equal(t, commit_key_data.Code, tracker.KeyCodeComplete)
 			assert.Equal(t, commit_key_data.State, tracker.KeyStateComplete)
 
-			file_key_data, file_key_exists := test_scan_repo.TrackerFiles.Get(response.Object.ID)
+			file_key_data, file_key_exists := s.TrackerFiles.Get(response.Object.ID)
 			assert.Truef(t, file_key_exists, "failed to find file ID in files tracker : ID=%s", response.Object.ID)
 			assert.Contains(t, file_key_data.Children, response.ID)
 			assert.Equal(t, file_key_data.Code, tracker.KeyCodeComplete)
@@ -473,22 +385,11 @@ func TestScanner_scanRepository(t *testing.T) {
 		{
 			config_func:  test_valid_git_config_func,
 			ctx:          test_context,
-			name:         "Scanner_Scan_Fail_Repo_URL",
-			err_chan:     make(chan error),
-			err_expected: errors.New("failed to parse repo name : invalid path in URL"),
-		},
-		{
-			config_func:  test_valid_git_config_func,
-			ctx:          test_context,
 			name:         "Scanner_Scan_Panic_Channel_Nil",
 			err_chan:     nil,
 			err_expected: nil,
 		},
 	}
-
-	// TODO
-	restore_tracker_commits := make(tracker.KeyDataMap)
-	restore_tracker_files := make(tracker.KeyDataMap)
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
@@ -509,8 +410,6 @@ func TestScanner_scanRepository(t *testing.T) {
 						repository,
 						nil,
 						make(chan struct{}),
-						restore_tracker_commits,
-						restore_tracker_files,
 					)
 				})
 				return
@@ -520,9 +419,8 @@ func TestScanner_scanRepository(t *testing.T) {
 				repository,
 				test.err_chan,
 				make(chan struct{}),
-				restore_tracker_commits,
-				restore_tracker_files,
 			)
+			t.Logf("waiting for error on channel")
 			err := <-test.err_chan
 
 			if test.err_expected == nil {
